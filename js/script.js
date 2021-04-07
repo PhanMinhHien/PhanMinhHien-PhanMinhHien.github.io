80954dae6ff93f15db82660fef87a6d66397066d
Minh Hien <minhhien1911997@gmail.com>
2021-04-07 16:21:06 +0700

    upload cv cá nhân

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+  CSS Document
+	Bootstrap 4 CSS
+	Author: Phan Minh Hiền
+	Date Create: 23/11/2020
+*/
+$('document').ready(function() {
+    $('#slider-field').slick({
+        arrows: false,
+        dots: false,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
